865bfc4cb95553938cdb06688930e95727c59603
Ang Xu <angxu1234@gmail.com>
2019-05-14 13:48:31 -0500

    7 out of 10 complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+const logo = document.querySelector("header img");
+logo.addEventListener("dblclick", function() {
+  alert("Don't click me!");
+});
+
+const title = document.querySelector(".logo-heading");
+title.addEventListener("mouseover", function() {
+  title.style.color = "green";
+});
+
+title.addEventListener("mouseout", function() {
+  title.style.color = "black";
+});
+
+const navItems = document.querySelectorAll("nav a");
+document.addEventListener("keydown", function() {
+  navItems.forEach(el => (el.style.color = "blue"));
+});
+
+document.addEventListener("scroll", function() {
+  navItems.forEach(el => (el.style.color = "black"));
+});
+
+const destImg = document.querySelector(".content-destination img");
+destImg.addEventListener("mouseenter", function() {
+  destImg.style.border = "thin solid red";
+});
+
+destImg.addEventListener("mouseleave", function() {
+  destImg.style.border = "none";
+});
