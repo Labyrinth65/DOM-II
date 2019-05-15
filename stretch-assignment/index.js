dc590c21b7e6271bc2aac2cb17bb96f1eb69eeac
Ang Xu <angxu1234@gmail.com>
2019-05-15 08:17:17 -0500

    fixed stretch assignment reset, can't figure out resetting position when clicking back down

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -9,28 +9,7 @@ rockets.forEach(el =>
 
 //mouse clicked moves box to the right
 let holding = false;
-
-rockets.forEach(el => el.addEventListener("mousedown", moveRight));
-
-window.addEventListener("mouseup", stopMove);
-
 let interval = null;
-
-function moveRight(e) {
-  holding = true;
-  e.target.currentX = 0; //how to fix this
-  interval = setInterval(function() {
-    updatePos(e.target);
-  }, 100);
-}
-
-function stopMove(e) {
-  holding = false;
-  clearInterval(interval);
-  interval = setInterval(function() {
-    updatePos(e.target);
-  }, 100);
-}
 
 function updatePos(e) {
   if (holding === true && e.currentX < 500) {
@@ -38,9 +17,60 @@ function updatePos(e) {
     e.style.position = "relative";
     e.style.left = e.currentX + "px";
   }
+}
+
+function returnPos(e) {
   if (holding === false && e.currentX > 0) {
     e.currentX = e.currentX - 10;
-    e.style.position = "relative";
     e.style.left = e.currentX + "px";
   }
+}
+
+function moveRight(e) {
+  holding = true;
+  e.target.currentX = 0;
+  interval = setInterval(function() {
+    updatePos(e.target);
+  }, 100);
+}
+
+rockets.forEach(el => el.addEventListener("mousedown", moveRight));
+
+//mouseup returns rockets
+window.addEventListener("mouseup", stopMove);
+
+// const rocketRed = document.querySelector(".block--red");
+// const rocketBlue = document.querySelector(".block--blue");
+// const rocketGreen = document.querySelector(".block--green");
+// const rocketPink = document.querySelector(".block--pink");
+// const rocketGray = document.querySelector(".block--gray");
+
+// function stopMove() {
+//   holding = false;
+//   clearInterval(interval);
+//   interval = setInterval(function() {
+//     returnPos(rocketRed);
+//   }, 100);
+//   interval = setInterval(function() {
+//     returnPos(rocketBlue);
+//   }, 100);
+//   interval = setInterval(function() {
+//     returnPos(rocketGreen);
+//   }, 100);
+//   interval = setInterval(function() {
+//     returnPos(rocketPink);
+//   }, 100);
+//   interval = setInterval(function() {
+//     returnPos(rocketGray);
+//   }, 100);
+// }
+
+function stopMove() {
+  holding = false;
+  clearInterval(interval);
+  rockets.forEach(function(el) {
+    interval = setInterval(function() {
+      returnPos(el);
+    }, 100);
+  });
 }
